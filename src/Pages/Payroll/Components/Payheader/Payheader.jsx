d84cadba0d0754fe7908d14822caf9e252c5b173
jerry <rago.d.e.v123@gmail.com>
2025-02-03 08:44:29 +0300

    made the top section of payroll responsive

--- src/Pages/Payroll/Components/Payheader/Payheader.jsx
+++ src/Pages/Payroll/Components/Payheader/Payheader.jsx
@@ -5,19 +5,21 @@ function Payheader() {
     return (
         <>
             <div class="form-container">
-                <select id="station" name="station">
-                    <option value="" disabled selected>please select</option>
-                    <option value="station1">Station 1</option>
-                    <option value="station2">Station 2</option>
-                    <option value="station3">Station 3</option>
-                </select>
+                <div className="select-opt">
+                    <select id="station" name="station">
+                        <option value="" disabled selected>please select</option>
+                        <option value="station1">Station 1</option>
+                        <option value="station2">Station 2</option>
+                        <option value="station3">Station 3</option>
+                    </select>
 
-                <select id="payroll-period" name="payroll-period">
-                    <option value="" disabled selected>please select</option>
-                    <option value="period1">Payroll Period 1</option>
-                    <option value="period2">Payroll Period 2</option>
-                    <option value="period3">Payroll Period 3</option>
-                </select>
+                    <select id="payroll-period" name="payroll-period">
+                        <option value="" disabled selected>please select</option>
+                        <option value="period1">Payroll Period 1</option>
+                        <option value="period2">Payroll Period 2</option>
+                        <option value="period3">Payroll Period 3</option>
+                    </select>
+                </div>
 
                 <input type="text" id="salary-slip-title" name="salary-slip-title" placeholder="Salary Slip Title" />
             </div>
