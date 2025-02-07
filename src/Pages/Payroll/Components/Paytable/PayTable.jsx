e566fa4adebaf530f4c94e15c7a36a300cf645a0
jerry <rago.d.e.v123@gmail.com>
2025-02-07 22:10:59 +0300

    added API to payroll

--- src/Pages/Payroll/Components/Paytable/PayTable.jsx
+++ src/Pages/Payroll/Components/Paytable/PayTable.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from 'antd/es/table';
 import { createStyles } from 'antd-style';
 import './Paytable.css';
-import dataSource from './datasource';
+import { base_url } from '../../../../constant'
+import { useNavigate } from 'react-router-dom';
+
+
 const useStyle = createStyles(({ css, token }) => {
   const { antCls } = token;
   return {
@@ -31,117 +34,68 @@ const columns = [
   {
     title: 'Employee',
     width: 160,
-    dataIndex: 'name',
-    key: 'name',
+    dataIndex: 'employee_name',
+    key: 'employee_name',
     fixed: 'left',
   },
   {
     title: 'Basic Salary',
     width: 150,
-    dataIndex: 'age',
-    key: 'age',
+    dataIndex: 'basic_salary',
+    key: 'basic_salary',
     fixed: 'left',
     sorter: true,
   },
   {
     title: 'Taxable Income',
-    dataIndex: 'address',
-    key: '1',
+    dataIndex: 'taxable_income',
+    key: 'taxable_income',
+    width: 150,
   },
   {
     title: 'Total',
-    dataIndex: 'address',
-    key: '2',
+    dataIndex: 'total_deductions',
+    key: 'total_deductions',
+    width: 90,
   },
   {
     title: 'NSSF Employee',
-    dataIndex: 'address',
-    key: '3',
-  },
-  {
-    title: 'NSSF Employer',
-    dataIndex: 'address',
-    key: '4',
+    dataIndex: 'nssf_employee',
+    key: 'nssf_employee',
+    width: 150,
   },
   {
     title: 'NHIF Employee',
-    dataIndex: 'address',
-    key: '5',
-  },
-  {
-    title: 'NHIF Employer',
-    dataIndex: 'address',
-    key: '6',
+    dataIndex: 'nhif_employee',
+    key: 'nhif_employee',
+    width: 150,
   },
   {
     title: 'PAYEE',
-    dataIndex: 'address',
-    key: '7',
-  },
-  {
-    title: 'Food Allowance',
-    dataIndex: 'address',
-    key: '8',
-  },
-  {
-    title: 'Advance',
-    dataIndex: 'address',
-    key: '9',
-  },
-  {
-    title: 'Short',
-    dataIndex: 'address',
-    key: '10',
+    dataIndex: 'paye',
+    key: 'paye',
+    width: 80,
   },
   {
     title: 'Staff Loan',
-    dataIndex: 'address',
-    key: '11',
-  },
-  {
-    title: 'SDL',
-    dataIndex: 'address',
-    key: '12',
-  },
-  {
-    title: 'WCF',
-    dataIndex: 'address',
-    key: '13',
-  },
-  {
-    title: 'Total',
-    dataIndex: 'address',
-    key: '14',
-  },
-  {
-    title: 'Net Pay',
-    dataIndex: 'address',
-    key: '15',
-  },
-  {
-    title: 'NHIF Number',
-    dataIndex: 'address',
-    key: '16',
-  },
-  {
-    title: 'NSSF Number',
-    dataIndex: 'address',
-    key: '17',
+    dataIndex: 'loan',
+    key: 'loan',
+    width: 150,
   },
   {
     title: 'TIN',
-    dataIndex: 'address',
-    key: '18',
+    dataIndex: 'tin',
+    key: 'tin',
   },
   {
     title: 'Bank A/C Number',
-    dataIndex: 'address',
-    key: '19',
+    dataIndex: 'bank_account',
+    key: 'bank_account',
   },
   {
     title: 'Bank Name',
-    dataIndex: 'address',
-    key: '20',
+    dataIndex: 'bank_name',
+    key: 'bank_name',
   },
   {
     title: 'Edit',
@@ -153,22 +107,82 @@ const columns = [
       if (record && Object.keys(record).length > 0) {
         return <a>action</a>;
       }
-      // Return null if the row has no data
       return null;
     },
   },
 ];
 
-const tableHeight = 1000;
+
+const PayTable = () => {
+  const { styles } = useStyle();
+
+  const token = localStorage.getItem('siteToken');
+const expiry = localStorage.getItem('siteExpiry')
+const navigate = useNavigate();
+
+const [dataSource, setDataSource] = useState([]);
+const [loading, setLoading] = useState(true)
+
+useEffect(() => {
+  const checkExpiry = () => {
+    if (Date.now() / 1000 > expiry) {
+      navigate("/");
+      return true;
+    }
+    return false;
+  };
+
+  const fetchPay = async () => {
+    if (checkExpiry()) return; 
+
+    try {
+      const response = await fetch(`${base_url}/auth/payroll`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const data = await response.json(); 
+
+      const formattedData = data.map((item, index) => ({
+        key: index, 
+        basic_salary: item.basic_salary,
+        tin: item.tin,
+        bank_name: item.bank_name,
+        bank_account: item.bank_account,
+        employee_name: item.employee_name,
+        taxable_income: item.taxable_income,
+        paye: item.paye,
+        loan: item.loan,
+        total_deductions: item.total_deductions,
+        nssf_employee: item.nssf_employee,
+        nhif_employee: item.nhif_employee,
+      }));
+
+      setDataSource(formattedData);
+    } catch (error) {
+      console.error("Error fetching employees:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  fetchPay();
+}, [base_url, token, expiry, navigate]); 
+
+const tableHeight = 1100;
 const rowHeight = 50;
 const emptyRowCount = Math.max(0, Math.floor(tableHeight / rowHeight) - dataSource.length);
 
 // Add empty rows to the data
 const paddedData = [...dataSource, ...Array(emptyRowCount).fill({})];
-const PayTable = () => {
-  const { styles } = useStyle();
   return (
-    <div style={{height: tableHeight}}>
+    <div style={{ height: tableHeight }}>
       <Table
         className={styles.customTable}
         pagination={false}
@@ -178,6 +192,7 @@ const PayTable = () => {
           x: 'max-content',
           y: tableHeight,
         }}
+        loading={loading}
       />
     </div>
   );
