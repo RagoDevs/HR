96ae0d7f8820fbe9b63ce09f192a7ab84ab73f5e
jerry <rago.d.e.v123@gmail.com>
2025-02-23 08:41:28 +0300

    changed login icon

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,8 +6,8 @@ import * as Yup from "yup";
 import { useAuth } from '../../RoutesAuth/AuthProvider';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import eyeIcon from '../../Assets/login/watch.png'
-import eyeSlashIcon from '../../Assets/login/hidden.png'
+import show from '../../Assets/login/show.png'
+import eyeSlashIcon from '../../Assets/login/hide.png'
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
@@ -21,25 +21,25 @@ function Login() {
     };
 
 
-    
+
     const isTokenValid = () => {
         const token = localStorage.getItem('siteToken');
         const expiryTime = localStorage.getItem('siteExpiry');
 
         if (!token || !expiryTime) {
             return false;
-          }
-      
-          const now = new Date().getTime();
-          return now / 1000 < expiryTime;
+        }
+
+        const now = new Date().getTime();
+        return now / 1000 < expiryTime;
     };
 
     useEffect(() => {
         if (isTokenValid()) {
-          // Redirect to dashboard if token is valid
-          navigate('/dashboard');
+            // Redirect to dashboard if token is valid
+            navigate('/dashboard');
         }
-      }, [navigate]);
+    }, [navigate]);
 
     const [loginError, setLoginError] = useState(null);
     let auth = useAuth();
@@ -77,18 +77,18 @@ function Login() {
                 {/*<div className="logo-intro-container">
                     <img src={logo} alt="Logo" />
                 </div>*/}
-                
-                    <div className="login">
-                        <div className="lg-container">
-                            <div className="lg-logo">
-                                <img src={logo} alt="" />
-                                <h2>Welcome Back!</h2>
-                            </div>
 
-                            <div className="lg-form">
-                                <form onSubmit={(e) => { e.preventDefault(); formik.handleSubmit() }}>
-                                    <div className='login--inputs'>
+                <div className="login">
+                    <div className="lg-container">
+                        <div className="lg-logo">
+                            <img src={logo} alt="" />
+                            <h2>Welcome Back!</h2>
+                        </div>
 
+                        <div className="lg-form">
+                            <form onSubmit={(e) => { e.preventDefault(); formik.handleSubmit() }}>
+                                <div className='login--inputs'>
+                                    <div className="email-wrapper">
                                         <input
                                             type='email'
                                             className='input'
@@ -99,45 +99,46 @@ function Login() {
                                             onBlur={formik.handleBlur}
                                             required
                                         />
-                                        {formik.touched.email && formik.errors.email ? (
-                                            <div>{formik.errors.email}</div>) : null
-                                        }
-                                        <div className="password-wrapper">
-                                            <input
-                                                type={showPassword ? 'text' : 'password'}
-                                                className='input'
-                                                placeholder='Password'
-                                                name='password'
-                                                value={formik.values.password}
-                                                onChange={formik.handleChange}
-                                                onBlur={formik.handleBlur}
-                                                required
-                                            />
-                                            <img
-                                                src={showPassword ? eyeSlashIcon : eyeIcon}
-                                                alt="Toggle Password Visibility"
-                                                className="eye-icon"
-                                                onClick={togglePasswordVisibility}
-                                            />
-                                        </div>
-                                        {formik.touched.password && formik.errors.password ? (
-                                            <div>{formik.errors.password}</div>) : null
-                                        }
-                                        {loginError && <div className="login-error">{loginError}</div>}
-                                        <div className='forgot--password'> Forgot Password?</div>
-                                        <button
-                                            type='submit'
-                                            className='lg-submit'
-                                        >
-                                            <h3>Login</h3>
-                                        </button>
                                     </div>
+                                    {formik.touched.email && formik.errors.email ? (
+                                        <div>{formik.errors.email}</div>) : null
+                                    }
+                                    <div className="password-wrapper">
+                                        <input
+                                            type={showPassword ? 'text' : 'password'}
+                                            className='input'
+                                            placeholder='Password'
+                                            name='password'
+                                            value={formik.values.password}
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                            required
+                                        />
+                                        <img
+                                            src={showPassword ? eyeSlashIcon : show}
+                                            alt="Toggle Password Visibility"
+                                            className="eye-icon"
+                                            onClick={togglePasswordVisibility}
+                                        />
+                                    </div>
+                                    {formik.touched.password && formik.errors.password ? (
+                                        <div>{formik.errors.password}</div>) : null
+                                    }
+                                    {loginError && <div className="login-error">{loginError}</div>}
+                                    <div className='forgot--password'> Forgot Password?</div>
+                                    <button
+                                        type='submit'
+                                        className='lg-submit'
+                                    >
+                                        <h3>Login</h3>
+                                    </button>
+                                </div>
 
-                                </form>
-                            </div>
+                            </form>
                         </div>
                     </div>
-                
+                </div>
+
             </div>
         </>
     )
--- src/Pages/Payroll/Components/Createpay/Createpay.jsx
+++ src/Pages/Payroll/Components/Createpay/Createpay.jsx
@@ -5,8 +5,6 @@ import { useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const { Option } = Select
-
 const Createpay = () => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
